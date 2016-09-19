58ccc6c8a0e9eed997d7a3445f35a26c6c4847af
AnupreetaMishra <mishra.anuprita@gmail.com>
2016-09-19 14:55:09 +0530

    grid

---
+++ Madhvi/Gridview1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Gridview1.aspx.cs" Inherits="Madhvi.Gridview" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" OnRowEditing="OnRowEditing" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+            <Columns>
+                <asp:BoundField DataField="Name" HeaderText="Name" ItemStyle-Width="150" />
+                <asp:BoundField DataField="Country" HeaderText="Country" ItemStyle-Width="150" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton Text="Edit" runat="server" CommandName="Edit" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:LinkButton Text="Update" runat="server" OnClick="OnUpdate" />
+                        <asp:LinkButton Text="Cancel" runat="server" OnClick="OnCancel" />
+                    </EditItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Madhvi/Gridview1.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Madhvi
+{
+    public partial class Gridview : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+            {
+            
+                 if (!this.IsPostBack)
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Columns.AddRange(new DataColumn[3] { new DataColumn("Id"), new DataColumn("Name"), new DataColumn("Country") });
+                        dt.Rows.Add(1, "John Hammond", "United States");
+                        dt.Rows.Add(2, "Mudassar Khan", "India");
+                        dt.Rows.Add(3, "Suzanne Mathews", "France");
+                        dt.Rows.Add(4, "Robert Schidner", "Russia");
+                        ViewState["dt"] = dt;
+                        this.BindGrid();
+                    }
+            }
+ 
+            protected void BindGrid()
+            {
+                 GridView1.DataSource = ViewState["dt"] as DataTable;
+                 GridView1.DataBind();
+            }
+
+            protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+            {
+        
+            }
+
+            protected void OnRowEditing(object sender, GridViewEditEventArgs e)
+            {
+             GridView1.EditIndex = e.NewEditIndex;
+             this.BindGrid();
+            }
+            protected void OnUpdate(object sender, EventArgs e)
+            {
+                GridViewRow row = (sender as LinkButton).NamingContainer as GridViewRow;
+                string name = (row.Cells[0].Controls[0] as TextBox).Text;
+                string country = (row.Cells[1].Controls[0] as TextBox).Text;
+                DataTable dt = ViewState["dt"] as DataTable;
+                dt.Rows[row.RowIndex]["Name"] = name;
+                dt.Rows[row.RowIndex]["Country"] = country;
+                ViewState["dt"] = dt;
+                GridView1.EditIndex = -1;
+                this.BindGrid();
+            }
+ 
+            protected void OnCancel(object sender, EventArgs e)
+            {
+                GridView1.EditIndex = -1;
+                this.BindGrid();
+            }
+        }
+    }
+
